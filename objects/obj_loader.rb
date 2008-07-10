3a6c08414245fb38305b45300f62db7b44e009d4
ear <e4rache@gmail.com>
2008-07-10 11:40:15 +0200

    Where we have strong emotions, we're liable to fool ourselves. - Carl Sagan (1934 - 1996),

--- objects/obj_loader.rb
+++ objects/obj_loader.rb
@@ -13,12 +13,13 @@ include Gl
 
 
 class Face
-	attr_accessor	:n,	:v
-	@n = [] # 3 Integers ( normal ) 
-	@v = [] # 3 Integers ( the 3 vertices index )
-	def initialize( v, n )
+	attr_accessor	:n,	:v, :type
+	@n = [] # 3-4 Integers ( normal ) 
+	@v = [] # 3-4 Integers ( the 3 vertices index )
+	def initialize( v, n , type)
 		@v = v
 		@n = n
+		@type = type
 	end
 end
 
@@ -57,18 +58,33 @@ class WaveFront < Entity3d
 		#glBegin(GL_QUADS)
 		#glBegin(GL_POINTS)
 		#glBegin(GL_LINES)
-		glBegin(GL_TRIANGLES)
 		@faces.each { |face|
-			glNormal( @normals[ face.n[0] ] )
-			glVertex( @vertices[ face.v[0] ] )
-		
-			glNormal( @normals[ face.n[1] ] )
-			glVertex( @vertices[ face.v[1] ] )
-
-			glNormal( @normals[ face.n[2] ] )
-			glVertex( @vertices[ face.v[2] ] )
+			if face.type == GL_TRIANGLES
+				glBegin(GL_TRIANGLES)
+				glNormal( @normals[ face.n[0] ] )
+				glVertex( @vertices[ face.v[0] ] )
+			
+				glNormal( @normals[ face.n[1] ] )
+				glVertex( @vertices[ face.v[1] ] )
+	
+				glNormal( @normals[ face.n[2] ] )
+				glVertex( @vertices[ face.v[2] ] )
+			else
+				glBegin(GL_QUADS)
+				glNormal( @normals[ face.n[0] ] )
+				glVertex( @vertices[ face.v[0] ] )
+			
+				glNormal( @normals[ face.n[1] ] )
+				glVertex( @vertices[ face.v[1] ] )
+	
+				glNormal( @normals[ face.n[2] ] )
+				glVertex( @vertices[ face.v[2] ] )
+				
+				glNormal( @normals[ face.n[3] ] )
+				glVertex( @vertices[ face.v[3] ] )
+			end
+			glEnd()
 		}
-		glEnd()
 		
 		glPopMatrix()
 	end
@@ -120,11 +136,22 @@ class WaveFront < Entity3d
 					vn = [ line_array[1].to_f, line_array[2].to_f, line_array[3].to_f ]
 					@normals << vn
 				when "f"  # face
-					tmp = line_array[1].split("/") + line_array[2].split("/") + line_array[3].split("/")
-					vertex_index_array = [tmp[0].to_i-1,tmp[3].to_i-1,tmp[6].to_i-1]
-					normal_index_array = [tmp[2].to_i-1,tmp[5].to_i-1,tmp[8].to_i-1]
-					f = Face.new( vertex_index_array, normal_index_array)
-					@faces << f
+					case line_array.size
+						when 4 # triangle
+							puts " Triangle : #{line_array.inspect}"
+							tmp = line_array[1].split("/") + line_array[2].split("/") + line_array[3].split("/")
+							vertex_index_array = [tmp[0].to_i-1,tmp[3].to_i-1,tmp[6].to_i-1]
+							normal_index_array = [tmp[2].to_i-1,tmp[5].to_i-1,tmp[8].to_i-1]
+							f = Face.new( vertex_index_array, normal_index_array,GL_TRIANGLES)
+							@faces << f
+						when 5 # quad
+							puts " Quad : #{line_array.inspect}"
+							tmp = line_array[1].split("/") + line_array[2].split("/") + line_array[3].split("/") + line_array[4].split("/")
+							vertex_index_array = [ tmp[0].to_i-1, tmp[3].to_i-1, tmp[6].to_i-1, tmp[9].to_i-1 ]
+							normal_index_array = [ tmp[2].to_i-1, tmp[5].to_i-1, tmp[8].to_i-1, tmp[10].to_i-1 ]
+							f = Face.new( vertex_index_array, normal_index_array,GL_QUADS)
+							@faces << f
+					end
 			end
 		end
 	end
--- test_trogle.rb
+++ test_trogle.rb
@@ -6,13 +6,16 @@ require "objects/cube_tex.rb"
 
 require "objects/obj_loader.rb"
 
-g = Trogl.new(800,600,120)
+g = Trogl.new(200,200,120)
 
 cube_tex = CubeTex.new()
 cube_tex.pos=[-3,0,0]
 
 wf_rock = WaveFront.new("data/obj/rock.obj")	
 wf_rock.pos=[-0,0,0]
+
+wf_plane1 = WaveFront.new("data/obj/toyplane.obj")
+wf_plane1.pos=[0,0,0]
 
 g.draw_axis=true
 g.target_fps=25
@@ -35,7 +38,7 @@ g.bind_key(SDL::Key::F2		, Proc.new {anim_obj = wf_rock} )
 
 g.entities.push(wf_rock)
 g.entities.push(cube_tex)
-
+g.entities.push(wf_plane1)
 SDL::TTF.init
 font = SDL::TTF.open("data/ft/verdana.ttf",24)
 font.style = SDL::TTF::STYLE_NORMAL
